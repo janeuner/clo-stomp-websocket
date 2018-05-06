7266f9e5e9a983e6f93bda86ba138d233b385dad
Jarod Neuner <jarod@neuner.us>
2018-05-06 13:33:48 -0500

    Complete list of location/weapon/suspect entities to GameEntityId.

--- core/src/main/java/us/neuner/clo/common/GameEntityId.java
+++ core/src/main/java/us/neuner/clo/common/GameEntityId.java
@@ -2,20 +2,56 @@ package us.neuner.clo.common;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+/*
+ * @author Mag Faris <mfaris627@gmail.com>
+ * @author Jarod Neuner <jarod@neuner.us>
+ * Enumerates the CLO game entities, including locations, weapons, and suspects.
+ */
 public enum GameEntityId {
 	InvalidValue("Invalid Value"),
-	
 	MissScarlet("Miss Scarlet"),
+	MrsWhite("Mrs White"),
+	MrGreen("Mr Green"),
+	MrsPeacock("Mrs Peacock"),
+	ProfessorPlum("Professor Plum"),
+	ColonelMustard("Colonel Mustard"), 
 
 	LeadPipe("Lead Pipe"),
+	CandleStick("Candle Stick"),
+	Rope("Rope"),
+	Knife("Knife"),
+	Wrench("Wrench"),
+	Revolver ("Revolver"),
 
 	BilliardRoom("Billiard Room"),
+	Kitchen("Kitchen"),
+	BallRoom("Ball Room"),
+	Conservatory("Conservatory"),
+	Library("Library"),
+	Study("Study"),
+	Hall("Hall"), 
+	Lounge ("Lounge"),
+	DiningRoom ("Dining Room"),
 
 	H01("Hallway 01"),
-
-	MissScarletStart("Start - Miss Scarlet")
-	;
-	//TODO: fill out remaining GameEntityIds
+	H02("Hallway 02"), 
+	H03("Hallway 03"), 
+	H04("Hallway 04"),
+	H05("Hallway 05"), 
+	H06("Hallway 06"), 
+	H07("Hallway 07"),
+	H08("Hallway 08"), 
+	H09("Hallway 09"), 
+	H10("Hallway 10"),
+	H11("Hallway 11"),
+	H12("Hallway 12"),
+	
+	MissScarletStart("Start - Miss Scarlet"),
+    MrsWhiteStart("Start - Mrs White"), 
+	MrGreenStart("Start - Mr Green"),
+	MrsPeacockStart("Start - Mrs Peacock"), 
+	ProfessorPlumStart ("Start Professor Plum"),
+	ColonelMustardStart("Start - Colonel Mustard");
 	
 	private final String text;
 	
@@ -23,10 +59,6 @@ public enum GameEntityId {
 		this.text = text;
 	}
 	
-	/*
-	 * Returns a string representation of the object. 
-	 * @see java.lang.Enum#toString()
-	 */
 	@JsonValue
 	@Override
 	public String toString() {
