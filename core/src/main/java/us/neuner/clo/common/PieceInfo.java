8aa09d7bde7fe7da1e0d9e707998349ce04db7c8
Raynor4000 <https://Raynor4000@github.com>
2018-05-11 00:40:38 -0700

    added rudimentary code to enforce house layout

--- core/src/main/java/us/neuner/clo/common/PieceInfo.java
+++ core/src/main/java/us/neuner/clo/common/PieceInfo.java
@@ -149,4 +149,125 @@ public class PieceInfo {
 		}
 		return false;
 	}
+	
+	public static void moveTo(GameBoardLocation loc, Boolean b) {
+		if (b==True) {
+			this.setLoc(location)
+		}
+		else if (this.isSuspect()){
+			switch (this.loc.toString()) {
+			case "Study":
+				if(location.equals("Hallway 01") || location.equals("Hallway 03") || location.equals("Kitchen")) {
+					this.setLoc(location)
+				}
+			case "Hall":
+				if(location.equals("Hallway 01") || location.equals("Hallway 02") || location.equals("Hallway 03")) {
+					this.setLoc(location)
+				}
+			case "Lounge":
+				if(location.equals("Hallway 02") || location.equals("Hallway 05") || location.equals("Conservatory")) {
+					this.setLoc(location)
+				}
+			case "Library":
+				if(location.equals("Hallway 03") || location.equals("Hallway 06") || location.equals("Hallway 08")) {
+					this.setLoc(location)
+				}
+			case "Billiard Room":
+				if(location.equals("Hallway 04") || location.equals("Hallway 06") || location.equals("Hallway 07")) || location.equals("Hallway 09") {
+					this.setLoc(location)
+				}
+			case "Dining Room":
+				if(location.equals("Hallway 05") || location.equals("Hallway 07") || location.equals("Hallway 10")) {
+					this.setLoc(location)
+				}
+			case "Convervatory":
+				if(location.equals("Hallway 08") || location.equals("Hallway 11") || location.equals("Lounge")) {
+					this.setLoc(location)
+				}
+			case "Ballroom":
+				if(location.equals("Hallway 11") || location.equals("Hallway 09") || location.equals("Hallway 12")) {
+					this.setLoc(location)
+				}
+			case "Kitchen":
+				if(location.equals("Hallway 12") || location.equals("Hallway 10") || location.equals("Study")) {
+					this.setLoc(location)
+				}
+			case "Hallway 01":
+				if(location.equals("Study") || location.equals("Hall")) {
+					this.setLoc(location)
+				}
+			case "Hallway 02":
+				if(location.equals("Hall") || location.equals("Lounge")) {
+					this.setLoc(location)
+				}
+			case "Hallway 03":
+				if(location.equals("Study") || location.equals("Library")) {
+					this.setLoc(location)
+				}
+			case "Hallway 04":
+				if(location.equals("Hall") || location.equals("Billiard Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 05":
+				if(location.equals("Lounge") || location.equals("Dining Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 06":
+				if(location.equals("Library") || location.equals("Billiard Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 07":
+				if(location.equals("Billiard Room") || location.equals("Dining Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 08":
+				if(location.equals("Library") || location.equals("Conservatory")) {
+					this.setLoc(location)
+				}
+			case "Hallway 09":
+				if(location.equals("Ballroom") || location.equals("Billiard Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 10":
+				if(location.equals("Kitchen") || location.equals("Dining Room")) {
+					this.setLoc(location)
+				}
+			case "Hallway 11":
+				if(location.equals("Conservatory") || location.equals("Ballroom")) {
+					this.setLoc(location)
+				}
+			case "Hallway 12":
+				if(location.equals("Ballroom") || location.equals("Kitchen")) {
+					this.setLoc(location)
+				}
+			case "Start - Miss Scarlet":
+				if(location.equals("Hallway 02")) {
+					this.setLoc(location)
+				}
+			case "Start - Mrs White":
+				if(location.equals("Hallway 12")) {
+					this.setLoc(location)
+				}
+			case "Start - Mr Green":
+				if(location.equals("Hallway 11")) {
+					this.setLoc(location)
+				}
+			case "Start - Mrs Peacock":
+				if(location.equals("Hallway 08")) {
+					this.setLoc(location)
+				}
+			case "Start Professor Plum":
+				if(location.equals("Hallway 03")) {
+					this.setLoc(location)
+				}
+			case "Start - Colonel Mustard":
+				if(location.equals("Hallway 05")) {
+					this.setLoc(location)
+				}
+			}
+		}
+		else if (this.isWeapon()){
+			
+		}
+	}
 }
