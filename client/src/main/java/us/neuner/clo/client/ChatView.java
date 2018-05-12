ce6396cb553bec019eb6280bd145369441eea64b
Jarod Neuner <jarod@neuner.us>
2018-05-12 14:17:27 -0500

    Enable chat in swing-client (hopefully)

--- client/src/main/java/us/neuner/clo/client/ChatView.java
+++ client/src/main/java/us/neuner/clo/client/ChatView.java
@@ -5,11 +5,19 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+
+import us.neuner.clo.message.ChatEntry;
+import us.neuner.clo.message.ChatMessage;
+import us.neuner.clo.message.ChatMessageHistory;
+import us.neuner.clo.message.ErrorMessage;
+import us.neuner.clo.message.GameSetupMessage;
+import us.neuner.clo.message.Message;
 
 public class ChatView extends JPanel{/**
 	 * serialization class version number
@@ -25,6 +33,31 @@ public class ChatView extends JPanel{/**
 	private JPanel messageSend = new JPanel();
 	private PlayerInfo playerSendingMessage;
 	private String playerName;
+	
+	private static class ChatMessageHandler implements CloServerMessageNotifier {
+
+		private final ChatView parent;
+		
+		ChatMessageHandler(ChatView parent) {
+			this.parent = parent;
+		}
+		
+		@Override
+		public void onMessageReceived(Message msg) {
+
+			if (msg instanceof ChatMessageHistory) {
+				ChatMessageHistory cmh = (ChatMessageHistory)msg;
+
+				parent.chatHistory.setText("");
+				for (int idx = 0; idx < cmh.getEntryCount(); idx++) {
+					ChatEntry ce = cmh.getEntry(idx);
+					String txt = String.format("[%s] %s\n", ce.getPlayerName(), ce.getMsg());
+					parent.chatHistory.append(txt);
+				}
+			}
+		}
+		
+	}	
 	
 
 	public ChatView(PlayerInfo player) {
@@ -44,10 +77,12 @@ public class ChatView extends JPanel{/**
 		
 		send.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				chatHistory.append(playerName + ": " + chatMessage.getText() + "\n");
+				//chatHistory.append(playerName + ": " + chatMessage.getText() + "\n");
+				CloApplication.sendChat(chatMessage.getText());
 			}
 		});
 
+		CloApplication.registerMessageNotifier(new ChatMessageHandler(this));
 	}
 
 
